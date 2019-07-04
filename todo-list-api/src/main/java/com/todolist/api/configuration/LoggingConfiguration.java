f312e2c1d8dbfecbbfa6f4c70eca546899fa2982
Chu-Ya Chia <chuyachia@gmail.com>
2019-07-03 23:03:48 -0400

    Use application.yml instead of application.properties, different configs on profile, log

---
+++ todo-list-api/src/main/java/com/todolist/api/configuration/LoggingConfiguration.java
@@ -0,0 +1,18 @@
+package com.todolist.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LoggingConfiguration {
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter =  new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
+}
